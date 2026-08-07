1c0d67c6eb1c282ee9b10d74e050773d9e609741
Henrincode <henrincode@gmail.com>
2026-08-06 22:24:53 -0300

    feat: create componet icon

---
+++ src/components/form/label/styles.ts
@@ -0,0 +1,9 @@
+import appColors from "@/styles/colors";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    fontSize: 14,
+    color: appColors.text
+  }
+})
