a6663371d1259accadf116852c6f11465639f318
Henrincode <spellctro@gmail.com>
2026-08-07 13:54:36 -0300

    feat: starting Logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import Icon from "../Icon";
+
+export default function Logo() {
+    return (
+        <View>
+            <Icon name="hammer-screwdriver" />
+            <Text>SOS</Text>
+        </View>
+    )
+}
