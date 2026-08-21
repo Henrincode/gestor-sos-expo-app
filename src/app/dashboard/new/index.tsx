d91f84bc2a6c2944cd61b598eced3f8775a4ce37
Henrincode <henrincode@gmail.com>
2026-08-20 21:14:44 -0300

    feat: start create new OS

--- src/app/dashboard/new/index.tsx
+++ src/app/dashboard/new/index.tsx
@@ -1,13 +1,42 @@
+import Input from "@/components/Form/Input";
+import InputGroup from "@/components/Form/InputGroup";
+import Label from "@/components/Form/Label";
 import Scroll from "@/components/Scroll";
-import { Text } from "react-native";
+import tw from "@/styles/tailwindColors";
+import { useState } from "react";
+import { Text, View } from "react-native";
 import styles from "./styles";
 
 export default function Index() {
+
+  const [teste, setTeste] = useState('')
+
   return (
     <Scroll nav style={styles.container}>
-      <Text>
-        Olá Mundo!
-      </Text>
+      {/* header */}
+      <View style={{ width: '100%', paddingHorizontal: 10 }}>
+        <Text style={{ fontSize: 24 }}>
+          Nova ordem de serviço
+        </Text>
+        <Text style={{ fontSize: 16, fontWeight: 'light', color: tw.gray['400'] }}>
+          Preencha os detalhes para iniciar um novo atendimento
+        </Text>
+      </View>
+      {/* form */}
+      <View style={{ gap: 20, paddingHorizontal: 10 }}>
+        <Text style={{ fontSize: 20 }}>
+          Preencha o formulário
+        </Text>
+        <InputGroup>
+          <Label icon="note-edit-outline" text="Título do serviço" />
+          <Input
+            value={teste}
+            setValue={setTeste}
+            placeholder="Ex: Reparo interruptor mesa 5"
+            style={{ backgroundColor: 'white' }}
+          />
+        </InputGroup>
+      </View>
     </Scroll>
   )
 }
